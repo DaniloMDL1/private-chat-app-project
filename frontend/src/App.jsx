8869f15035b7c09b63ff6cdf2940cfebc42211e1
DaniloMDL1 <danilodfl016@gmail.com>
2024-04-07 22:41:36 +0200

    socket url update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
 
   useEffect(() => {
     if(user && !socket) {
-      const socketInstance = io("http://localhost:3001", {
+      const socketInstance = io("https://private-chat-app-bg6b.onrender.com/", {
         query: {
           userId: user._id
         }
